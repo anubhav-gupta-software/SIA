cb58d6a0114a639eccce3989f5ce4f62d3f2f00b
anubhav-gupta-software <anubhavgupta.main@gmail.com>
2024-08-10 00:37:33 +0530

    comments

--- about.js
+++ about.js
@@ -1,3 +1,18 @@
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Navbar hide/show on scroll
+//     let lastScrollTop = 0;
+//     const navbar = document.querySelector('.navbar');
+
+//     window.addEventListener('scroll', function() {
+//         let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//         if (scrollTop > lastScrollTop) {
+//             navbar.style.top = '-70px';  // Hide the navbar
+//         } else {
+//             navbar.style.top = '0';  // Show the navbar
+//         }
+//         lastScrollTop = scrollTop;
+//     });
+// });
 document.addEventListener("DOMContentLoaded", function() {
     // Load the loading screen
     fetch('loading.html')
